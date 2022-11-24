726730b4a78225514bc8edb6439259ed1a1f0be4
Mustafa <mustafaanandwala@gmail.com>
2022-11-25 04:06:50 +0530

    Moving pieces

--- engine.py
+++ engine.py
@@ -27,3 +27,45 @@ class GameState:
         ]
         self.white_to_move = True
         self.log_moves = []
+
+    def makeMove(self, move):
+        self.board[move.start_row][move.start_column] = "--"
+        self.board[move.end_row][move.end_column] = move.piece_moved
+        self.log_moves.append(move)  # Log the move
+        self.white_to_move = not self.white_to_move
+
+
+class Move:
+    # mapping our board to match the chess board notation
+    ranks_to_rows = {'1': 7, '2': 6, '3': 5, '4': 4,
+                     '5': 3, '6': 2, '7': 0}
+    rows_to_rank = {
+        v: k
+        for k, v in ranks_to_rows.items()
+    }
+    files_to_columns = {'a': 0, 'b': 1, 'c': 2, 'd': 3,
+                        'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+    columns_to_files = {
+        v: k
+        for k, v in files_to_columns.items()
+    }
+
+    # So we make this class to make moves
+    # It starts with a start quare and then the player plays and then the end square
+    # We also pass board to check the validity of the move
+    # We now have all the records of a player's move in one place
+    def __init__(self, start_square, end_square, board):
+        self.start_row = start_square[0]
+        self.start_column = start_square[1]
+        self.end_row = end_square[0]
+        self.end_column = end_square[1]
+        self.piece_moved = board[self.start_row][self.start_column]
+        self.piece_captured = board[self.end_row][self.end_column]
+
+    def get_chess_notations(self):
+        return self.get_rank_files(self.start_row, self.start_column) + self.get_rank_files(self.end_row,
+                                                                                            self.end_column)
+
+    def get_rank_files(self, rows, columns):
+        return self.columns_to_files[columns] + self.rows_to_rank[rows]
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ GameState object.
 
 import pygame as p
 from engine import GameState
+from engine import Move
 
 # Defining global variables
 WIDTH = 512
@@ -40,12 +41,46 @@ def main():
     game_state = GameState()
     load_images()
 
+    # This will keep track of last click of the user
+    # This is above the for loops as we are going to use this tuple to store data (row and column)
+    # No square is selected initially
+    square_selected = ()
+
+    # This will keep track of where the player has clicked
+    # This will have two tuples stored. Eg: [(6, 4), (4, 4)]
+
+    player_click = []
+
     # To run the game on loop and once the user quit, flip the table
     running = True
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            # We click the piece and it moves
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                # (x,y) location of the mouse
+                col = location[0] // SQUARE_SIZE
+                row = location[1] // SQUARE_SIZE
+
+                # To check if the user has clicked the same square twice
+                # If they do select the same square, we empty the tuple i.e. deselects
+                # Also reset the player_clicks
+                if square_selected == (row, col):
+                    square_selected = ()
+                    player_click = []
+                else:
+                    square_selected = (row, col)
+                    player_click.append(square_selected)  # Append both first and second clicks
+
+                if len(player_click) == 2:
+                    move = Move(player_click[0], player_click[1], game_state.board)
+                    print(move.get_chess_notations())  # Debugging purposes
+                    game_state.makeMove(move)
+                    square_selected = ()  # reset the user clicks
+                    player_click = []
+
             draw_game_state(screen, game_state)
             clock.tick(MAX_FPS)
             p.display.flip()
